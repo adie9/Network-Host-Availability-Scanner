e4f8620cb2582bc0d2c8eac004c88897a2b8e894
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2026-05-26 16:17:05 -0400

    Added base load_hosts and ping_hosts functions

--- scanner.py
+++ scanner.py
@@ -3,8 +3,28 @@ from pythonping import ping
 HOSTS = "data/hosts.txt"
 OUTPUT = "output.txt"
 
+
+def load_hosts(file_path):
+    with open(file_path, "w") as f:
+        hosts = []
+        for line in f:
+            line.strip()
+            if line:
+                hosts.append(line)
+    return hosts
+
+
+def ping_hosts(ip):
+    response = ping(ip, verbose=True)
+    if response.success():
+        print("The ping was successful!")
+    else:
+        print("The ping failed. Host was unreachable.")
+
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
